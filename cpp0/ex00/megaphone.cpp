62761e1495ee8d8c96444081861c22e4cdabbb62
theoske <urufudjd@gmail.com>
2022-12-20 17:05:03 +0100

    ex00

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: theo <theo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 17:32:14 by theo              #+#    #+#             */
-/*   Updated: 2022/12/19 17:55:05 by theo             ###   ########.fr       */
+/*   Updated: 2022/12/20 17:04:32 by theo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,39 @@
 	
 	$>./megaphone
 	* LOUD AND UNBEARABLE FEEDBACK NOISE *
-	
-	$>
 */
 
-void	upgrade(int argc, char **argv)
+void	megaphone(char **argv)
 {
-	
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 1;
+	while (argv[i])
+	{
+		str = argv[i];
+		j = 0;
+		while (str[j])
+		{
+			if (str[j] > 96 && str[j] < 123)
+				std::cout << (char)(str[j] - 32);
+			else if (str[j] != '\"')
+				std::cout << str[j];
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	if (argc > 2)
-		upgrade(argc, argv);
-	else
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	megaphone(argv);
 	return 0;
 }
