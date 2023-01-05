81a472c6b8513bb94b6bfeb7876c6e410be4d5b8
Theo Kempf elbaz <tkempf-e@k0r3p8.42mulhouse.fr>
2023-01-05 18:52:39 +0100

    update ex04

--- cpp1/ex04/main.cpp
+++ cpp1/ex04/main.cpp
@@ -6,12 +6,37 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 17:38:17 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/01/05 17:40:04 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/01/05 18:52:18 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+
 int	main(int argc, char *argv[])
 {
-	
+	std::string		filename;
+	std::string		file2name;
+	std::string 	s1;
+	std::string		s2;
+
+	if (argc != 4)
+	{
+		std::cout << "argument error" << std::endl;
+		return 0;
+	}
+	filename = argv[1];
+	file2name = filename;
+	file2name.insert(file2name.length(), ".replace");
+	s1 = argv[2];
+	s2 = argv[3];
+	std::ifstream	myfile(filename);
+	std::ofstream	file2(file2name);
+	//copier jusqua occu s1
+	//quand occu mettre s2
+	myfile.close();
+	file2.close();
 	return (0);
 }
