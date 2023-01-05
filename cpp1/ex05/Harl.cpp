39fb67472d2c624e7c9a941dcda05338b1d09fb9
Theo Kempf elbaz <tkempf-e@k0r3p7.42mulhouse.fr>
2023-01-05 20:56:02 +0100

    update

--- cpp1/ex05/Harl.cpp
+++ cpp1/ex05/Harl.cpp
@@ -6,7 +6,41 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 20:15:05 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/01/05 20:15:06 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/01/05 20:40:17 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do !" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl << "I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	
+}
--- cpp1/ex05/Harl.hpp
+++ cpp1/ex05/Harl.hpp
@@ -6,7 +6,29 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 20:14:53 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/01/05 20:15:08 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/01/05 20:31:52 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class	Harl
+{
+	private :
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public :
+		Harl();
+		~Harl();
+
+		void	complain(std::string level);
+};
+
+#endif
