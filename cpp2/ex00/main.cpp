a4b5048d5e49bfb2193c95bc4b35ae40ad0d1b2c
Theo Kempf elbaz <tkempf-e@k0r2p11.42mulhouse.fr>
2023-02-03 15:32:14 +0100

    operator update

--- cpp2/ex00/main.cpp
+++ cpp2/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 17:45:57 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/01/06 18:05:34 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/02/03 15:31:30 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main()
 	Fixed	b(a);
 	Fixed	c;
 
-	c = b;
+	c = b;//surcharge operateur affection
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
