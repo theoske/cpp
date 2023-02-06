17ca70b262acb9b5d90ab59600614d21cf2a3861
Theo Kempf elbaz <tkempf-e@k0r3p10.42mulhouse.fr>
2023-02-06 14:43:35 +0100

    harl update

--- cpp1/ex05/main.cpp
+++ cpp1/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 20:14:26 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/01/06 17:17:50 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/02/06 14:40:11 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ int	main()
 	Harl	harl;
 
 	harl.complain("debug");
+	sleep(2);
 	harl.complain("info");
+	sleep(2);
 	harl.complain("warning");
+	sleep(2);
 	harl.complain("error");
 	return (0);
 }
