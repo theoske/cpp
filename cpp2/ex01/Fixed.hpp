60e59c5478e968e49fb341585c226baa1c04088f
Theo Kempf elbaz <tkempf-e@k0r3p10.42mulhouse.fr>
2023-02-10 15:27:44 +0100

    ex01 update

--- cpp2/ex01/Fixed.hpp
+++ cpp2/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 15:38:47 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/02/03 15:44:50 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/02/10 15:07:27 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,17 @@ class Fixed
 		const static int	bits;
 	public:
 		Fixed(void);
-		Fixed(Fixed &tocopy);
+		Fixed(const Fixed &tocopy);
 		Fixed(const int cnb);
 		Fixed(const float cfnb);
 		~Fixed();
 		Fixed&operator=(const Fixed &f);
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 };
+
+std::ostream&	operator<<(std::ostream& os, const Fixed &f);
 
 #endif
--- cpp2/ex01/main.cpp
+++ cpp2/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 15:32:46 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/02/03 15:40:29 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/02/04 16:28:03 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 int	main()
 {
-	Fixed	a;
-	Fixed	b(a);
-	Fixed	c;
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
 
-	c = b;//surcharge operateur affection
-
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
-
-	return (0);
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return 0;
 }
