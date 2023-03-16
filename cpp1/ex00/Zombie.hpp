3e26493d8d1bee0af69fd30479688ee6e8f61d04
Theo Kempf elbaz <tkempf-e@k0r3p9.42mulhouse.fr>
2023-03-16 17:40:42 +0100

    ex00 update

--- cpp1/ex00/Zombie.hpp
+++ cpp1/ex00/Zombie.hpp
@@ -3,32 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   Zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: theo <theo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 13:30:52 by theo              #+#    #+#             */
-/*   Updated: 2023/01/03 15:19:32 by theo             ###   ########.fr       */
+/*   Updated: 2023/03/16 17:39:44 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+# define	ZOMBIE_HPP
+
 #include <iostream>
+#include <string>
 
 class Zombie
 {
 	private :
 		std::string	name;
 	public :
-		void announce(void)
-		{
-			std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-			return ;
-		};
-		void naming(std::string newname)
-		{
-			Zombie::name = newname;
-		};
 		Zombie(void);
 		~Zombie(void);
+		Zombie(const Zombie &f);
+		Zombie&operator=(const Zombie &f);
+
+		void announce(void);
+		void naming(std::string newname);
 };
 
 void	randomChump(std::string name);
 Zombie	*newZombie(std::string name);
+
+#endif
