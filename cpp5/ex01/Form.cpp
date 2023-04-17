4e43c7d727772159a24d432ebd1c481af750e2d6
Theo Kempf elbaz <tkempf-e@k0r2p9.42mulhouse.fr>
2023-04-17 18:07:34 +0200

    first page

--- cpp5/ex01/Form.cpp
+++ cpp5/ex01/Form.cpp
@@ -6,18 +6,18 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:20:25 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/04/17 15:41:49 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/04/17 18:06:48 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form() : name("N/A"), grade_to_exe(150), grade_to_sign(150)
+Form::Form() : name("N/A"), grade_to_exe(150), grade_to_sign(150), is_signed(0)
 {
 	std::cout << "Default Form constructor called" << std::endl;
 }
 
-Form::Form(const std::string name, int grade_to_exe, int grade_to_sign) : name(name), grade_to_exe(grade_to_exe), grade_to_sign(grade_to_sign)
+Form::Form(const std::string name, int grade_to_exe, int grade_to_sign) : name(name), grade_to_exe(grade_to_exe), grade_to_sign(grade_to_sign), is_signed(0)
 {
     std::cout << "Form constructor called" << std::endl;
     try
@@ -50,17 +50,37 @@ Form::~Form()
 void	Form::GradeTooHighException()
 {
     std::cout << "Grade too high exception called" << std::endl;
-	if (this->grade_to_sign > 150)
-    	this->grade_to_sign = 150;
-	else
-		this->grade_to_exe = 150;
 }
 
 void	Form::GradeTooLowException()
 {
     std::cout << "Grade too low exception called" << std::endl;
-	if (this->grade_to_sign < 1)
-    	this->grade_to_sign = 1;
-	else
-		this->grade_to_exe = 1;
+}
+
+std::string	Form::getName()
+{
+	return (this->name);
+}
+
+const int	Form::getGradeSign()
+{
+	return (this->grade_to_sign);
+}
+
+const int	Form::getGradeExe()
+{
+	return (this->grade_to_exe);
+}
+
+std::string	Form::isSigned()
+{
+	if (this->is_signed == 1)
+		return ("yes");
+	return ("no");
+}
+
+std::ostream& operator<<(std::ostream &os, Form &f)
+{
+	os << f.getName() << ", form grade to sign: " << f.getGradeSign() << " grade to execute: " << f.getGradeExe() << " signed status: " << f.isSigned() << std::endl;
+	return os;
 }
--- cpp5/ex01/Form.hpp
+++ cpp5/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:10:19 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/04/17 15:41:38 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/04/17 18:06:43 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,14 @@ public:
 	Form&operator=(const Form &f);
 	~Form();
 
-	void	GradeTooLowException();
-	void	GradeTooHighException();
+	void		GradeTooLowException();
+	void		GradeTooHighException();
+	const int	getGradeSign();
+	const int	getGradeExe();
+	std::string	getName();
+	std::string	isSigned();
 };
+
+std::ostream& operator<<(std::ostream &os, Form &f);
 
 #endif
