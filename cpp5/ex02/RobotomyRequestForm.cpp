e52a1c7ef1bff0be3fc4b1e4d83530184aed6425
Theo Kempf elbaz <tkempf-e@k0r3p10.42mulhouse.fr>
2023-04-20 15:31:29 +0200

    up

--- cpp5/ex02/RobotomyRequestForm.cpp
+++ cpp5/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,13 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 16:17:36 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/04/19 16:17:37 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/04/20 15:30:23 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : name("N/A"), is_signed(0), grade_to_sign(72), grade_to_exe(45)
+{
+	std::cout << "Default RobotomyRequestForm constructor called" << std::endl;
+}
--- cpp5/ex02/RobotomyRequestForm.hpp
+++ cpp5/ex02/RobotomyRequestForm.hpp
@@ -6,7 +6,28 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 16:17:39 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/04/19 16:17:40 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/04/20 15:29:47 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ROBO_HPP
+# define ROBO_HPP
+
+# include "Form.hpp"
+
+class RobotomyRequestForm : public Form
+{
+	private:
+		std::string const 	name;
+		bool				is_signed;
+		int const			grade_to_sign;
+		int const			grade_to_exe;
+	public:
+		RobotomyRequestForm();
+		RobotomyRequestForm(const std::string name, std::string target);
+		RobotomyRequestForm(const RobotomyRequestForm &f);
+		RobotomyRequestForm &operator=(const RobotomyRequestForm &f);
+		virtual ~RobotomyRequestForm();
+};
+
+#endif
