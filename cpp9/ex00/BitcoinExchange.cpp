87122f2c1cc51c76dc110e4798de6ff1c1ff39e3
Theo Kempf elbaz <tkempf-e@k0r2p10.42mulhouse.fr>
2023-05-13 15:00:11 +0200

    no error handling

--- cpp9/ex00/BitcoinExchange.cpp
+++ cpp9/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:39:31 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/05/12 17:40:41 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/05/13 14:59:19 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,119 +50,19 @@ void	BitcoinExchange::get_data()
 		inputMap.insert(std::make_pair(line.substr(0, line.find(" ")), line.substr(line.find("| ") + 1)));
 	while (std::getline(this->data, line))
 		dataMap.insert(std::make_pair(line.substr(0, line.find(",")), line.substr(line.find(",") + 1)));
-	// for (it = inputMap.begin(); it != inputMap.end(); it++)
-    // 	std::cout << "Date: " << it->first << ", Data: " << it->second << std::endl;
 	for (std::map<std::string, std::string>::const_iterator it = inputMap.begin(); it != inputMap.end(); it++)
 	{
 		date = it->first;
 		value = it->second;
 
 		it2 = dataMap.find(date);
+		//error: wrong date, no amount, 0 < amount < 1000
 		if (it2 != dataMap.end())
 		{
 			long double value2 = std::stold(it2->second);
 			long double multiplication = std::stold(value) * value2;
-			std::cout << "Date: " << date << ", Data: " << value << ", Data2: " << value2 << ", Multiplication: " << multiplication << std::endl;
+			std::cout << "Date: " << date << ", btc amount: " << value << ", btc value: " << value2 << ", Multiplication: " << multiplication << std::endl;
 		}
 	}
 }
-
-// void	BitcoinExchange::addTransaction(std::string const &date, float amount, std::string const &from, std::string const &to)
-// {
-// 	std::string	transaction;
-
-// 	transaction = date + " " + std::to_string(amount) + " " + from + " " + to;
-// 	this->_transactions.push_back(transaction);
-// 	if (std::find(this->_wallets.begin(), this->_wallets.end(), from) == this->_wallets.end())
-// 		this->_wallets.push_back(from);
-// 	if (std::find(this->_wallets.begin(), this->_wallets.end(), to) == this->_wallets.end())
-// 		this->_wallets.push_back(to);
-// }
-
-// void	BitcoinExchange::printTransactions(std::string const &name) const
-// {
-// 	std::list<std::string>::const_iterator	it;
-// 	std::string								transaction;
-// 	std::string								date;
-// 	std::string								amount;
-// 	std::string								from;
-// 	std::string								to;
-
-// 	std::cout << "Transactions for " << name << ":" << std::endl;
-// 	for (it = this->_transactions.begin(); it != this->_transactions.end(); it++)
-// 	{
-// 		transaction = *it;
-// 		date = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		amount = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		from = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		to = transaction.substr(0, transaction.find(" "));
-// 		if (from == name || to == name)
-// 			std::cout << date << " " << amount << " " << from << " " << to << std::endl;
-// 	}
-// }
-
-// void	BitcoinExchange::printWallet(std::string const &name) const
-// {
-// 	std::list<std::string>::const_iterator	it;
-// 	std::string								transaction;
-// 	std::string								date;
-// 	std::string								amount;
-// 	std::string								from;
-// 	std::string								to;
-// 	float									balance;
-
-// 	balance = 0;
-// 	for (it = this->_transactions.begin(); it != this->_transactions.end(); it++)
-// 	{
-// 		transaction = *it;
-// 		date = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		amount = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		from = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		to = transaction.substr(0, transaction.find(" "));
-// 		if (from == name)
-// 			balance -= std::stof(amount);
-// 		if (to == name)
-// 			balance += std::stof(amount);
-// 	}
-// 	std::cout << name << " has " << balance << " BTC" << std::endl;
-// }
-
-// void	BitcoinExchange::printTransactions() const
-// {
-// 	std::list<std::string>::const_iterator	it;
-// 	std::string								transaction;
-// 	std::string								date;
-// 	std::string								amount;
-// 	std::string								from;
-// 	std::string								to;
-
-// 	std::cout << "Transactions:" << std::endl;
-// 	for (it = this->_transactions.begin(); it != this->_transactions.end(); it++)
-// 	{
-// 		transaction = *it;
-// 		date = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		amount = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		from = transaction.substr(0, transaction.find(" "));
-// 		transaction = transaction.substr(transaction.find(" ") + 1);
-// 		to = transaction.substr(0, transaction.find(" "));
-// 		std::cout << date << " " << amount << " " << from << " " << to << std::endl;
-// 	}
-// }
-
-// void	BitcoinExchange::printWallets() const
-// {
-// 	std::list<std::string>::const_iterator	it;
-
-// 	std::cout << "Wallets:" << std::endl;
-// 	for (it = this->_wallets.begin(); it != this->_wallets.end(); it++)
-// 		std::cout << *it << std::endl;
-// }
 
--- cpp9/ex00/BitcoinExchange.hpp
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:39:28 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/05/11 15:07:16 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/05/13 14:59:13 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,7 @@ class BitcoinExchange
 		BitcoinExchange	&operator=(BitcoinExchange const &src);
 
 		void	get_data();
-		// void	addTransaction(std::string const &date, float amount, std::string const &from, std::string const &to);
-		// void	printTransactions(std::string const &name) const;
-		// void	printWallet(std::string const &name) const;
-		// void	printWallets() const;
-		// void	printTransactions() const;
-		// void	printPrices(std::string) const;
+		
 
 	private:
 		std::list<std::string>		_wallets;
