64f344244d73c845f03a279ff6370ee399df07b0
Theo Kempf elbaz <tkempf-e@k2r1p1.42mulhouse.fr>
2023-05-15 16:28:49 +0200

    ex01 update

--- cpp6/ex01/main.cpp
+++ cpp6/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:00:00 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/05/15 16:25:56 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/05/15 16:27:48 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 int	main()
 {
 	Data* data = new Data();
+	
 	uintptr_t raw = Serializer::serialize(data);
 	Data* data2 = Serializer::deserialize(raw);
+	
 	std::cout << "Data2: " << data2 << std::endl;
 	std::cout << "Data: " << data << std::endl;
 	return (0);
