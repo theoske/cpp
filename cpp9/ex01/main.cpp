866420629f7199f5aeafdc524af26253deab1411
Theo Kempf elbaz <tkempf-e@k0r2p8.42mulhouse.fr>
2023-05-18 16:29:36 +0200

    ex01

--- cpp9/ex01/main.cpp
+++ cpp9/ex01/main.cpp
@@ -6,21 +6,21 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 16:03:10 by tkempf-e          #+#    #+#             */
-/*   Updated: 2023/05/18 16:12:24 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2023/05/18 16:24:49 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-int	main(int ac, char **av)
+int	main(int argc, char **argv)
 {
 	RPN	rpn;
 
-	if (ac == 2)
+	if (argc == 2)
 	{
 		try
 		{
-			rpn.compute(av[1]);
+			rpn.compute(argv[1]);
 		}
 		catch (std::exception &e)
 		{
